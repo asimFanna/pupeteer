4748be41a4795e39a816ab34164e350006ec6f8f
Asim Mohammed <asim@aqar.fm>
2024-05-05 22:12:24 +0300

    configure the browser

--- src/index.js
+++ src/index.js
@@ -9,4 +9,16 @@ async function actLikeAHuman() {
         args: ['--incognito',]
 
     }));
+
+
+    const [ page ] = await browser.pages();
+    page.setDefaultNavigationTimeout(0);
+    await page.setExtraHTTPHeaders({
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+        'upgrade-insecure-requests': '1',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'en-US,en;q=0.9,en;q=0.8'
+    });
+
 }
